9a22b4160f055f34539be90e24b4e705739b17ae
SamGCo <samgco11@gmail.com>
2023-04-19 00:07:30 -0400

    round 2

--- hw5Analysis.R
+++ hw5Analysis.R
@@ -1,10 +1,11 @@
 #1
 library(modelsummary)
 library(fixest)
+final.data<-subset(final.data,State != "District of Columbia" | State != "Puerto Rico")
 q1Data<-final.data%>%
   mutate(insured=ins_employer+ins_direct+ins_medicaid+ins_medicare)%>%
   group_by(year)%>%
-  summarize(q1Share=mean(ins_direct/insured))
+  summarize(q1Share=mean(ins_direct/adult_pop,na.rm=TRUE))
 ans1<-ggplot(q1Data, aes(x = year, y = q1Share)) +
   geom_line()+
   labs(x = "Year", y = "Share of Direct Purchase Insured Individuals", title = "Direct Purchase Share of Insured Over Time")
@@ -15,7 +16,7 @@ ans1
 q3Data<-final.data%>%
   mutate(insured=ins_employer+ins_direct+ins_medicaid+ins_medicare)%>%
   group_by(year)%>%
-  summarize(q3Share=mean(ins_medicaid/insured))
+  summarize(q3Share=mean(ins_medicaid/adult_pop,na.rm=TRUE))
 ans3<-ggplot(q3Data, aes(x = year, y = q3Share)) +
   geom_line()+
   labs(x = "Year", y = "Share of Medicaid Insured Individuals", title = "Medicaid Share of Insured Over Time")
@@ -71,34 +72,38 @@ mod.twfe <- fixest::feols(perc_unins~i(year, expand_ever, ref=2013) | State + ye
                   cluster=~State,
                   data=reg.dat)
 modelsummary(mod.twfe)
+ans9<-iplot(mod.twfe)
 library(broom)
-coefficients <- tidy(mod.twfe, conf.int = TRUE)[, c("term", "estimate")]
-
-tidy_results <- tidy(mod.twfe, conf.int = TRUE) %>%
-  select(term, estimate, std.error, conf.low, conf.high)
-years<-c(2012,2014,2015,2016,2017,2018,2019)
-tidy_results$term<-years
-ans9<-ggplot(tidy_results, aes(x = term, y = estimate)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2) +
-  labs(x = "Year", y = "Estimates and 95% CI", title = "Medicaid Expansion Effect on Uninsured Each Year")
+# coefficients <- tidy(mod.twfe, conf.int = TRUE)[, c("term", "estimate")]
+# 
+# tidy_results <- tidy(mod.twfe, conf.int = TRUE) %>%
+#   select(term, estimate, std.error, conf.low, conf.high)
+# 
+# years<-c(2012,2014,2015,2016,2017,2018,2019)
+# tidy_results$term<-years
+# ans9<-ggplot(tidy_results, aes(x = term, y = estimate)) +
+#   geom_point() +
+#   geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2) +
+#   labs(x = "Year", y = "Estimates and 95% CI", title = "Medicaid Expansion Effect on Uninsured Each Year")
 #10
 reg.dat10 <- final.data %>% 
   filter(!is.na(expand_ever)) %>%
   mutate(perc_unins=uninsured/adult_pop,
          post = (year>=2014), 
          treat=post*expand_ever,
-         time_to_treat = ifelse(expand_ever==FALSE, 0, year-expand_year),
-         time_to_treat = ifelse(time_to_treat < -3, -3, time_to_treat))
+         time_to_treat = ifelse(expand_ever==TRUE, year-expand_year,-1),
+         time_to_treat = ifelse(time_to_treat < -4, -4, time_to_treat))
 mod10 <- feols(perc_unins~i(time_to_treat, expand_ever, ref=-1) | State + year,
                   cluster=~State,
                   data=reg.dat10)
-tidy_results10 <- tidy(mod10, conf.int = TRUE) %>%
-  select(term, estimate, std.error, conf.low, conf.high)
-timeToTreat<-c("-3","-2","0","1","2","3","4","5")
-tidy_results10$term<-timeToTreat
-ans10<-ggplot(tidy_results10, aes(x = term, y = estimate)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2) +
-  labs(x = "Years to Treat", y = "Estimates and 95% CI", title = "Medicaid Expansion Effect on Uninsured Each Year")
+ans10<-iplot(mod10)
+# tidy_results10 <- tidy(mod10, conf.int = TRUE) %>%
+#   select(term, estimate, std.error, conf.low, conf.high)
+# timeToTreat<-c(-3,-2,0,1,2,3,4,5)
+# tidy_results10$term<-timeToTreat
+# ans10<-ggplot(tidy_results10, aes(x = term, y = estimate)) +
+#   geom_point() +
+#   geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2) +
+#   labs(x = "Years to Treat", y = "Estimates and 95% CI", title = "Medicaid Expansion Effect on Uninsured Each Year")
+# ans10
 save.image("workspace.RData")
